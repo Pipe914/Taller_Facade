5dc6e04b31d3a5cd7c4f876de86c0c7e3fe59ba9
ANDRES FELIPE AFANADOR SALAZAR <andresafsa@unisabana.edu.co>
2021-04-09 12:59:11 -0500

    Archivos Proxy

---
+++ Taller_5/src/main/java/com/canessa/maven/test/proxy/ILogin.java
@@ -0,0 +1,6 @@
+package com.canessa.maven.test.proxy;
+
+public interface ILogin {
+    public void verifyLogin();
+    public void createKey();
+}
---
+++ Taller_5/src/main/java/com/canessa/maven/test/proxy/Login.java
@@ -0,0 +1,10 @@
+package com.canessa.maven.test.proxy;
+
+public class Login implements ILogin{
+    public void verifyLogin(){
+
+    }
+    public void createKey(){
+        
+    }
+}
