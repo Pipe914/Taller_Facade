eb83bd6eb8f8ee03427611679bf76499d39a44e4
ANDRES FELIPE AFANADOR SALAZAR <andresafsa@unisabana.edu.co>
2021-04-22 18:18:50 -0500

    Codigo completamente comentado listo para subirse a Master

--- Taller_5/src/main/java/com/canessa/maven/test/oferta/OfertaBase.java
+++ Taller_5/src/main/java/com/canessa/maven/test/oferta/OfertaBase.java
@@ -2,13 +2,17 @@ package com.canessa.maven.test.oferta;
 
 import com.canessa.maven.test.Componente;
 
+// Clase base oferta
 public class OfertaBase implements Componente{
+// Variables globales
     private String id;
 
+// Constructor
     public OfertaBase(String iD){
         id = iD;
     }
     
+// Metodos de clase
     public void setId(String iD){
         id = iD;
     }
--- Taller_5/src/main/java/com/canessa/maven/test/oferta/TiempoOferta.java
+++ Taller_5/src/main/java/com/canessa/maven/test/oferta/TiempoOferta.java
@@ -2,15 +2,18 @@ package com.canessa.maven.test.oferta;
 
 import com.canessa.maven.test.Componente;
 
+// Clase propiedad oferta: Descripcion
 public class TiempoOferta extends ModificadorOferta{
-
+// Variables globales
     private String tiempo;
 
+// Constructor
     public TiempoOferta(Componente nuevaOferta, String tiempo) {
         super(nuevaOferta);
         this.tiempo = tiempo;
     }
 
+// Metodos de clase
     public String imprimirOferta(){
         return nuevaOferta.imprimirOferta() + addTiempoOferta();
     }
